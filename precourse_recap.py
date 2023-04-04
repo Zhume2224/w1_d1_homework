1349b5772789a2a686b8540b11fba25ef3f3f464
Zhu Meng <zhume2224@outlook.com>
2023-04-04 09:03:59 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,16 @@
+#print(type('hello'))
+#print(type(24))
+
+number_1=22
+number_2=13
+sum_of_numbers=number_1+number_2
+print(sum_of_numbers)
+print(1+int('2'))
+ # to convert str to num
+word_1="apple"
+word_2="bird"
+
+print(word_1+" "+word_2)
+
+#to convert using call brakets
+print(word_1.capitalize())
